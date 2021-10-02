69f4a5c9b8b061ca9a232e995fa2e8d08161ee9b
gsalazar1998 <gsalazar@tpitic.com.mx>
2021-10-02 09:21:46 -0700

    Añadido de iconos en inicio de sesion

--- vista/header.php
+++ vista/header.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Enclavezado icono flaticon-->
+    <link rel="icon" type="image/png" href="./css/icons/hand.png">
+
     <!--modificaciones de la pagina propias-->
     <link rel="stylesheet" href="./css/cobEstilos.css">
     <link rel="stylesheet" href="./bootstrap-4.3.1//css/bootstrap.min.css">
--- vista/login.php
+++ vista/login.php
@@ -2,7 +2,8 @@
 <div class="formLogin ">
 
 <form class="mt-5 mb-5 login-input" action="?c=cobrador&a=validar" method="POST">
-  <h4> Alta de cobradores </h4>  
+<img src="./css/icons/hand1.0.png">
+  <h4> Alta de cobradores </h4> 
 <label>usuario
 <input type="text" placeholder="Usuario" name="usuario"></label>
 <label>contraseña
